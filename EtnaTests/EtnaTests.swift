8241574a5b66c87e4b9dcdc8ee92cb7cd630e4cb
Tomasz <pengwius@protonmail.ch>
2024-10-01 19:45:00 +0200

    Init

---
+++ EtnaTests/EtnaTests.swift
@@ -0,0 +1,17 @@
+//
+//  EtnaTests.swift
+//  EtnaTests
+//
+//  Created by Tomasz on 01/10/2024.
+//
+
+import Testing
+@testable import Etna
+
+struct EtnaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ etna/EtnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtnaApp.swift
+//  Etna
+//
+//  Created by Tomasz on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct EtnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
